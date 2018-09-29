e93d585907ec624c8738730e59c4f5ad6ceacfd3
袁老三 <yhonglai@163.com>
2018-09-29 23:18:07 +0800

    自定义控件模块校验

--- SinaWeiboHouseKeeper/WeiboHouseKeeper/View/WeiboSetView.cs
+++ SinaWeiboHouseKeeper/WeiboHouseKeeper/View/WeiboSetView.cs
@@ -13,11 +13,185 @@ namespace WeiboHouseKeeper.View
 {
     public partial class WeiboSetView : Skin_DevExpress
     {
+        //图文微博固定频率
+        private uint imageWeiboFixedTime = 20;
+        public uint ImageWeiboFixedTime
+        {
+            get
+            {
+                return this.imageWeiboFixedTime;
+            }
+        }
+
+        //图文微博随机频率下限
+        private uint imageWeiboRandomTimeMin = 0;
+        public uint ImageWeiboRandomTimeMin
+        {
+            get
+            {
+                return this.imageWeiboRandomTimeMin;
+            }
+        }
+
+        //图文微博随机频率上限
+        private uint imageWeiboRandomTimeMax = 60;
+        public uint ImageWeiboRandomTimeMax
+        {
+            get
+            {
+                return this.imageWeiboRandomTimeMax;
+            }
+        }
+
+        //视频微博固定频率
+        private uint videoWeiboFixedTime = 20;
+        public uint VideoWeiboFixedTime
+        {
+            get
+            {
+                return this.videoWeiboFixedTime;
+            }
+        }
+
+        //视频微博随机频率下限
+        private uint videoWeiboRandomTimeMin = 0;
+        public uint VideoWeiboRandomTimeMin
+        {
+            get
+            {
+                return this.videoWeiboRandomTimeMin;
+            }
+        }
+
+        //视频微博随机频率上限
+        private uint videoWeiboRandomTimeMax = 60;
+        public uint VideoWeiboRandomTimeMax
+        {
+            get
+            {
+                return this.videoWeiboRandomTimeMax;
+            }
+        }
+
+        //每次自动关注人数
+        private uint autoFollowConut = 0;
+        public uint AutoFollowCount
+        {
+            get
+            {
+                return this.autoFollowConut;
+            }
+        }
+
+        //每次自动取消关注数
+        private uint autoUnFollowCount = 0;
+        public uint AutoUnFollowCount
+        {
+            get
+            {
+                return this.autoUnFollowCount;
+            }
+        }
+
+
         public WeiboSetView()
         {
             InitializeComponent();
         }
 
+        #region 私有方法
+        /// <summary>
+        /// 图文模块有效设置校验
+        /// </summary>
+        /// <returns>true:当前设置有效</returns>
+        private bool IsImageWeiboSettingValid()
+        {
+            bool isValid = true;
+            if (this.checkBoxImage.Checked)
+            {
+                if (this.radioImageFixed.Checked && !this.radioImageRandom.Checked)
+                {
+                    if (!UInt32.TryParse(this.textBoxImageFixed.Text, out this.imageWeiboFixedTime))
+                    {
+                        this.labelImageError1.Visible = true;
+                        isValid = false;
+                    }
+                }
+                else if (!this.radioImageFixed.Checked && this.radioImageRandom.Checked)
+                {
+                    if (!UInt32.TryParse(this.textBoxImageRandom1.Text, out this.imageWeiboRandomTimeMin) ||
+                        !UInt32.TryParse(this.textBoxImageRandom2.Text, out this.imageWeiboRandomTimeMax))
+                    {
+                        this.labelImageError2.Visible = true;
+                        isValid = false;
+                    }
+                    else if (this.imageWeiboRandomTimeMin >= this.imageWeiboRandomTimeMax)
+                    {
+                        this.labelImageError2.Visible = true;
+                        isValid = false;
+                    }
+                }
+                else
+                {
+                    this.labelImageError1.Visible = true;
+                    isValid = false;
+                }
+            }
+            return isValid;
+        }
+
+        /// <summary>
+        /// 视频模块有效设置校验
+        /// </summary>
+        /// <returns>true:当前设置有效; false:设置无效</returns>
+        private bool IsVideoWeiboSettingValid()
+        {
+            bool isValid = true;
+            if (this.checkBoxVideo.Checked)
+            {
+                if (this.radioVideoFixed.Checked && !this.radioVideoRandom.Checked)
+                {
+                    if (!UInt32.TryParse(this.textBoxVideoFixed.Text, out this.videoWeiboFixedTime))
+                    {
+                        this.labelVideoError1.Visible = true;
+                        isValid = false;
+                    }
+                }
+                else if (!this.radioVideoFixed.Checked && this.radioVideoRandom.Checked)
+                {
+                    if (!UInt32.TryParse(this.textBoxVideoRandom1.Text, out this.videoWeiboRandomTimeMin) ||
+                        !UInt32.TryParse(this.textBoxVideoRandom2.Text, out this.videoWeiboRandomTimeMax))
+                    {
+                        this.labelVideoError2.Visible = true;
+                        isValid = false;
+                    }
+                    else if (this.videoWeiboRandomTimeMin >= this.videoWeiboRandomTimeMax)
+                    {
+                        this.labelVideoError2.Visible = true;
+                        isValid = false;
+                    }
+                }
+                else
+                {
+                    this.labelVideoError1.Visible = true;
+                    isValid = false;
+                }
+            }
+            return isValid;
+        }
+
+        /// <summary>
+        ///清除所有错误提示
+        /// </summary>
+        private void HideErrorPoint()
+        {
+            this.labelImageError1.Visible = false;
+            this.labelImageError2.Visible = false;
+            this.labelVideoError1.Visible = false;
+            this.labelVideoError2.Visible = false;
+        }
+
+        #endregion
 
         #region 事件
 
@@ -140,16 +314,13 @@ namespace WeiboHouseKeeper.View
         private void buttonOK_Click(object sender, EventArgs e)
         {
             //图文微博设置模块校验
-            if (this.checkBoxImage.Checked)
+            if (!this.IsImageWeiboSettingValid() || !this.IsVideoWeiboSettingValid())
             {
-                if (this.radioImageFixed.Checked)
-                {
-
-                }
-                else
-                {
-
-                }
+                return;
+            }
+            else
+            {
+                this.HideErrorPoint();
             }
 
             //视频微博设置模块校验
