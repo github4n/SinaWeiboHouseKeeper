43add0170e9dbf4e47c2aa34c24dbe2db9cd3655
袁老三 <yhonglai@163.com>
2018-10-06 22:13:46 +0800

    添加控件

--- SinaWeiboHouseKeeper/Test/Form1.Designer.cs
+++ SinaWeiboHouseKeeper/Test/Form1.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.userLable1 = new WeiboHouseKeeper.UserLable();
-            this.userLable2 = new WeiboHouseKeeper.UserLable();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.userLable1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(471, 470);
+            this.panel1.TabIndex = 0;
             // 
             // userLable1
             // 
-            this.userLable1.Location = new System.Drawing.Point(12, 12);
+            this.userLable1.Location = new System.Drawing.Point(4, 4);
             this.userLable1.MaximumSize = new System.Drawing.Size(0, 170);
             this.userLable1.MinimumSize = new System.Drawing.Size(449, 190);
             this.userLable1.Name = "userLable1";
@@ -42,33 +53,35 @@
             this.userLable1.Size = new System.Drawing.Size(449, 190);
             this.userLable1.TabIndex = 0;
             // 
-            // userLable2
+            // button1
             // 
-            this.userLable2.Location = new System.Drawing.Point(12, 208);
-            this.userLable2.MaximumSize = new System.Drawing.Size(0, 170);
-            this.userLable2.MinimumSize = new System.Drawing.Size(449, 190);
-            this.userLable2.Name = "userLable2";
-            this.userLable2.NickName = "groupBox1";
-            this.userLable2.Size = new System.Drawing.Size(449, 190);
-            this.userLable2.TabIndex = 1;
+            this.button1.Location = new System.Drawing.Point(609, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(855, 495);
-            this.Controls.Add(this.userLable2);
-            this.Controls.Add(this.userLable1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Panel panel1;
         private WeiboHouseKeeper.UserLable userLable1;
-        private WeiboHouseKeeper.UserLable userLable2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- SinaWeiboHouseKeeper/Test/Form1.cs
+++ SinaWeiboHouseKeeper/Test/Form1.cs
@@ -15,6 +15,26 @@ namespace Test
         public Form1()
         {
             InitializeComponent();
+            this.controls.Add(this.userLable1);
+
+        }
+
+        List<UserControl> controls = new List<UserControl>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            WeiboHouseKeeper.UserLable uLabel = new WeiboHouseKeeper.UserLable();
+
+            uLabel.Location = new Point(4, this.panel1.Controls[this.panel1.Controls.Count - 1].Location.Y + 190 + 6);
+            uLabel.MaximumSize = new Size(0, 170);
+            uLabel.MinimumSize = new Size(449, 190);
+            uLabel.Name = "userLable2";
+
+            uLabel.Size = new System.Drawing.Size(449, 190);
+
+            this.panel1.Controls.Add(uLabel);
+
         }
     }
 }
